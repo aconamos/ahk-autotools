d9d60fa404045b79cf6efe9457ccb81f90817ad2
nnortonschool <nephi.norton@sastudent.com>
2022-03-23 12:00:26 -0600

    further build script changes

--- buildscript.ps1
+++ buildscript.ps1
@@ -1,14 +1,17 @@
-Write-Information "Removing items..."
+$version = "0.0.2"
+
+Write-Host "Removing items..."
 Remove-Item -Path ".\bin\*"
 
-Write-Information "Building..."
+Write-Host "Building..."
 $srcItems = Get-ChildItem -Path ".\src\" -Name
 $srcArray = $srcItems -split "\r\n"
 $basePath = Resolve-Path -Path ".\compiler\Unicode 64-bit.bin"
 
 foreach ($f in $srcArray) {
     $srcPath = ".\src\" + $f
-    ./compiler/Ahk2Exe.exe /in $srcPath /out ".\bin\" /base $basePath /silent
+    $binPath = ".\bin\" + $f.TrimEnd(".ahk") + "-" + $version + ".exe"
+    ./compiler/Ahk2Exe.exe /in $srcPath /out $binPath /base $basePath /silent
 }
 
-Write-Progress "Complete!"
+Write-Host "Complete!"
