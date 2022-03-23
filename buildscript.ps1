f31b4b429e71b8b0d9262e0ecb9def4d53e959c7
nnortonschool <nephi.norton@sastudent.com>
2022-03-23 11:52:29 -0600

    Build script magic

--- buildscript.ps1
+++ buildscript.ps1
@@ -2,6 +2,13 @@ Write-Information "Removing items..."
 Remove-Item -Path ".\bin\*"
 
 Write-Information "Building..."
-./compiler/Ahk2Exe.exe /in ".\src\autotools.ahk" /out ".\bin\autotools.exe" /base "C:\Users\student\Documents\clicker\compiler\Unicode 64-bit.bin"
+$srcItems = Get-ChildItem -Path ".\src\" -Name
+$srcArray = $srcItems -split "\r\n"
+$basePath = Resolve-Path -Path ".\compiler\Unicode 64-bit.bin"
+
+foreach ($f in $srcArray) {
+    $srcPath = ".\src\" + $f
+    ./compiler/Ahk2Exe.exe /in $srcPath /out ".\bin\" /base $basePath /silent
+}
 
 Write-Progress "Complete!"
